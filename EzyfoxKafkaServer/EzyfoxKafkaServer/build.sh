4d09d4c013e7973145e99d93ed53ac7313f2a4d7
toilahtc96 <toilahtc@gmail.com>
2021-11-13 16:30:18 +0700

    rename and fix deploy

--- EzyfoxKafkaServer/EzyfoxKafkaServer/build.sh
+++
@@ -1,6 +0,0 @@
-#export EZYFOX_SERVER_HOME=
-mvn -pl . clean install
-mvn -pl EzyfoxKafkaServer-common -Pexport clean install
-mvn -pl EzyfoxKafkaServer-app-api -Pexport clean install
-mvn -pl EzyfoxKafkaServer-app-entry -Pexport clean install
-mvn -pl EzyfoxKafkaServer-plugin -Pexport clean install
